c506a46774b6ee90bbd50c4f75e90e56d4e0fb3f
brandonwilliams <brandonjaegerwilliams@gmail.com>
2024-07-29 11:54:31 +0200

    expose-document-services: expose document services via rest

--- src/main/java/com/assessment/documentservice/controller/DocumentController.java
+++ src/main/java/com/assessment/documentservice/controller/DocumentController.java
@@ -1,9 +1,15 @@
 package com.assessment.documentservice.controller;
 
 import com.assessment.documentservice.core.security.CurrentUser;
-import com.assessment.documentservice.service.DocumentService;
+import com.assessment.documentservice.model.DocumentModel;
+import com.assessment.documentservice.service.ProcessedDocumentService;
+import com.assessment.documentservice.service.UnProcessedDocumentService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.Resource;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -15,16 +21,29 @@ import java.io.IOException;
 @RequestMapping("/v1/documents")
 public class DocumentController {
 
-    private final DocumentService documentService;
+    private final UnProcessedDocumentService unProcessedDocumentService;
+    private final ProcessedDocumentService processedDocumentService;
 
     @PostMapping
     public String uploadDocument(@RequestParam("file") MultipartFile file) throws IOException {
-        return documentService.upload(file);
+        return unProcessedDocumentService.upload(file);
     }
 
     @PostMapping("/process")
     @ResponseStatus(value = HttpStatus.NO_CONTENT)
     public void process(@CurrentUser UserDetails userDetails) {
-        documentService.process(userDetails);
+        unProcessedDocumentService.process(userDetails);
+    }
+
+    @GetMapping
+    public Page<DocumentModel> getDocuments(@RequestParam int page, @RequestParam int size) {
+        return processedDocumentService.getDocuments(page, size);
+    }
+
+    @GetMapping("/{id}/download")
+    public ResponseEntity<Resource> getDocument(@PathVariable Long id) throws IOException {
+        return ResponseEntity.ok()
+                .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                .body(processedDocumentService.download(id));
     }
 }
